edbd2bbfc663ff11c59ab48def493bc242e78137
miqueiasbelfort <kawasousa1554@gmail.com>
2022-03-31 20:56:11 -0300

    profile

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
           <Route path="/:username/:reponame" element={<Repo />}/>
       </Routes>
       {/*Footer*/}
+      
       <GlobalStyles />
     </BrowserRouter>
   )
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,31 @@
 import React from "react"
 
+import { Container, Main, LeftSide, RightSide  } from "./style"
+
+import ProfileData from "../../components/ProfileData"
+
 const Profile: React.FC = () => {
-    return <div></div>
+    return (
+        <Container>
+            <Main>
+                <LeftSide>
+                    <ProfileData
+                        username={"MiqueiasBelfort"}
+                        name={'Miqueias Belfort'}
+                        avatarUrl={"https://avatars.githubusercontent.com/u/79981066?v=4"}
+                        followers={877}
+                        following={7}
+                        company={"Nubank"}
+                        location={"Basília, Brazil"}
+                        email={"miqueiasbelfort8323@gmail.com"}
+                        blog={"https://miqueiasbelfor.netlefy.app"}
+                    />
+                </LeftSide>
+
+                <RightSide></RightSide>
+            </Main>
+        </Container>
+    )
 }
 
 export default Profile
